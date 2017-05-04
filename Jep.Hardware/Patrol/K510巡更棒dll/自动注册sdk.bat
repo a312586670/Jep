e3ba3557be898709e8417b8de276869ed3c67e4d
a312586670 <supper@9126.org>
2017-05-04 23:19:11 +0800

    modify

--- Jep.Hardware/Patrol/K510巡更棒dll/自动注册sdk.bat
+++
@@ -1,5 +0,0 @@
-copy .\*.dll %windir%\system32\
-copy .\*.ocx %windir%\system32\
-copy .\License.dat C:\
-regsvr32 %windir%\system32\MSCOMM32.OCX
-regsvr32 %windir%\system32\pskEx.ocx
--- Jep.Hardware/bin/Debug/自动注册sdk.bat
+++
@@ -1,3 +0,0 @@
-copy \IDR210\*.dll %windir%\system32\
-copy \IDR210\*.ocx %windir%\system32\
-
